c18a804f0214e4daa6cd6e37a813f3321fe97eec
Sohan <prajapatisohan800@gmail.com>
2026-07-24 17:46:42 +0530

    working on weather fit

---
+++ frontend/src/layout/web_layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function web_layout() {
+    return (
+        <div>
+            <div>side bar</div>
+            <div>
+                <div>top bar</div>
+                <div>content</div>
+            </div>
+        </div>
+    )
+}
